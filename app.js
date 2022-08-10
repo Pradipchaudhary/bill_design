2b6488f5319f5e0dbb9863933ab2562f802610ef
Pradip Chaudhary <chaudharypradip678@gmail.com>
2022-08-10 15:00:58 +0545

    final design

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
-function PrintDiv(id) {
-  var data = document.getElementById(id).innerHTML;
-  var myWindow = window.open("", "my div", "height=800,width=1200");
-  myWindow.document.write("<html><head><title>my div</title>");
-  myWindow.document.write(
-    '<link rel="stylesheet" href="style.css" type="text/css" />'
-  );
-  myWindow.document.write(
-    '<link rel="stylesheet" href="print.css" type="text/css" />'
-  );
-  myWindow.document.write("</head><body >");
-  myWindow.document.write(data);
-  myWindow.document.write("</body></html>");
-  myWindow.document.close(); // necessary for IE >= 10
-
-  myWindow.onload = function () {
-    // necessary if the div contain images
-
-    myWindow.focus(); // necessary for IE >= 10
-    myWindow.print();
-    myWindow.close();
+const onPrint = (element) => {
+  var divContents = document.getElementById(element);
+  var divContents1 = document.getElementById(element).cloneNode(true);
+  var copy = divContents1.querySelector("#copy");
+  copy.innerHTML = "Parent Copy";
+  var a = window.open("", "", "height=500, width=500");
+  var head = document.querySelector("head");
+  a.document.write("<html>");
+  a.document.write(head.innerHTML);
+  a.document.write('<body><div id="print-parent-div">');
+  a.document.write(divContents.innerHTML);
+  a.document.write('<div id="divider-div"></div>');
+  a.document.write(divContents1.innerHTML);
+  a.document.write("</div></body></html>");
+  a.document.close();
+  a.onload = function () {
+    a.onafterprint = a.close;
+    a.print();
   };
-}
+};
